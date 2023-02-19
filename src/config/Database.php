8449486110503c9981f94cf5a8c24788ca50427f
Keid Achird <keidachird@gmail.com>
2023-02-19 20:27:25 +0100

    Update Database.php

--- src/config/Database.php
+++ src/config/Database.php
@@ -15,9 +15,9 @@ class Database
     public function getConnection(): PDO
     {
         if ($this->conn === null) {
-            $dsn = "mysql:host=$this->host;dbname=$this->name;charset=utf8";
+            $dsn = "mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_784ca88e6d20f21;charset=utf8";
 
-            $this->conn = new PDO($dsn, $this->user, $this->password, [
+            $this->conn = new PDO($dsn, "bef553e2bd40da", "c5615c8e", [
                     PDO::ATTR_STRINGIFY_FETCHES => false,
                     PDO::ATTR_EMULATE_PREPARES => false,
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
